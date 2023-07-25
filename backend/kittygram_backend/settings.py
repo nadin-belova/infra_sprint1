ac012c11986b64c841a3393b928d10668597c365
Nadezhda <belovanadezhda1983@gmail.com>
2023-07-25 19:00:11 +0000

    static admin

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -96,7 +96,10 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
+#STATIC_ROOT = BASE_DIR / 'static_backend'
+STATIC_URL = 'static_backend'
+STATIC_ROOT = BASE_DIR / 'static_backend'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = '/var/www/kittygram/media'
